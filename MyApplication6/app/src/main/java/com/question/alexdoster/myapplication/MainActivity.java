e16cd33a549048bdec263958e357a8d8a42f6d35
adoster36 <alexdoster@alexs-MacBook-Pro.local>
2015-10-07 11:29:33 +0100

    initial commit

---
+++ MyApplication6/app/src/main/java/com/question/alexdoster/myapplication/MainActivity.java
@@ -0,0 +1,33 @@
+package com.question.alexdoster.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    //variables go here
+    private Button btnfalse;
+    private Button btntrue;
+    private TextView lblQuestion;
+    private ImageView imgpictures;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+
+
+    }
+
+
+}
