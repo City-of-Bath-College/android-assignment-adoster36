38c276a516619b5c1b9bd411fe80758f63036db3
adoster36 <alexdoster@alexs-MacBook-Pro.local>
2015-11-24 14:08:02 +0000

    7 changes
    profile card and questions

--- MyGeographyApp/app/src/main/java/com/question/alexdoster/mygeographyapp/Main23Activity.java
+++ MyGeographyApp/app/src/main/java/com/question/alexdoster/mygeographyapp/Main23Activity.java
@@ -114,4 +114,4 @@ public class MainActivity extends Activity {
 
     }
 }
-}
+
---
+++ MyGeographyApp/app/src/main/java/com/question/alexdoster/mygeographyapp/Main24Activity.java
@@ -0,0 +1,117 @@
+package com.question.alexdoster.mygeographyapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+public class MainActivity extends Activity {
+
+    //variables go here
+    private Button btnTrue;
+    private Button btnFalse;
+    private TextView lblQuestion;
+    private ImageView imgPicture;
+
+    private List<QuestionObject>questions;
+
+    private QuestionObject currentQuestions;
+    private int index;
+    private int score;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // connect variables to interface items
+        btnTrue = (Button) findViewById(R.id.btnTrue);
+        btnFalse = (Button) findViewById(R.id.btnFalse);
+        lblQuestion = (TextView) findViewById(R.id.lblQuestion);
+        imgPicture = (ImageView) findViewById(R.id.imgPicture);
+
+        //set geography question
+        lblQuestion.setText("is this the flag for Canada");
+
+        //set image Picture
+        imgPicture.setImageResource(R.drawable.canadianflag);
+
+        index = 0;
+        score = 0;
+
+        //onclick listners
+        btnTrue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "correct!!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        //onclick listners
+        btnFalse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "wrong!!", Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
+        generateQuestions();
+
+    }
+    private void generateQuestions(){
+
+        questions=new ArrayList<>();
+
+        questions.add(new QuestionObject("Is this the flag of canada,true,R.drawable.canadianflag "));
+        questions.add(new QuestionObject("Is this Aircraft an airbusa380,false,R.drawable.airbusa380"));
+        questions.add(new QuestionObject("Is this payment sytem known as apple pay,true,R.drawable.applepay"));
+        questions.add(new QuestionObject("Is this aircraft a boeing747,false,R.drawable.boeing747"));
+        questions.add(new QuestionObject("Where is the eiffeltower,true,R.drawable.eiffeltower"));
+        questions.add(new QuestionObject("Is this plane made in russia,false,R.drawable.russiancargoplane"));
+        questions.add(new QuestionObject("who makes this car,true,R.drawable.nissanskylinenismo"));
+        questions.add(new QuestionObject("which is the film that bumblebee the car starred in,false,R.drawable.bumblebee"));
+        questions.add(new QuestionObject("who makes this car,true,R.drawable.mercedes"));
+        questions.add(new QuestionObject("what is this tower called,false,R.drawable.leaningtowerofpisa"));
+
+    }
+
+    private void setUpQuestion(){
+
+        currentQuestions=questions.get(index);
+
+        lblQuestion.setText(currentQuestions.getquestion());
+        imgPicture.setImageResource(currentQuestions.getPicture());
+
+        index++;
+    }
+    private boolean determineButtonPress(boolean answer){
+
+        boolean expectedAnswer = currentQuestions.isAnswer();
+
+        if (answer == expectedAnswer){
+            //you were right!!
+            Toast.makeText(MainActivity.this,"correct", Toast.LENGTH_SHORT).show();
+        }else{
+            // you were wrong!!
+
+            Toast.makeText(MainActivity.this,"correct", Toast.LENGTH_SHORT).show();
+            score++;
+
+            if (super.onOptionsItemSelected(item)) return true;
+            else {
+                return false;
+            }
+        }
+        return expectedAnswer;
+    } static {
+
+    }
+}
+
